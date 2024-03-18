f9aaed9733bcff09a6816d3b799eb1c5e19de350
kdholakiya <kdholakiya>
2024-03-18 13:03:34 +0530

    UG-1683: using callback

--- us-ik/service.js
+++ us-ik/service.js
@@ -1,14 +1,13 @@
-let leadScoreReponse = {};
-function leadScoreService() {
+function leadScoreService(params, callback) {
   const API_KEY = "cT8YFc92uylyqfQXWMMrFdLTiMA";
   const API = "https://ajoably4nhpvq73kbzfkxmfnuu0eixdq.lambda-url.us-west-1.on.aws";
-  const utmparams = getAllUrlParams();
+  const utmparams = params?.utmparams;
 
   const data = JSON.stringify({
     email: $('.invitee_email').val(),
     formatted_date: $('.event_start_time').val(),
     lead_email: $('.invitee_email').val(),
-    channel: decodeURIComponent(utmparams['utm_source']),
+    channel: decodeURIComponent(utmparams?.['utm_source']),
     role_domain: $('.role_domain').val(),
     work_ex: $('.work_experience').val(),
     interview_start_time: $('.int_start_time').val(),
@@ -22,21 +21,18 @@ function leadScoreService() {
     alumni_stats: "New_Lead",
   });
 
-  console.log("Lead score payload", data)
-
   $.ajax({
     method: "POST",
     url: API,
     data: data,
     headers: { 'x-api-key': API_KEY, 'Content-Type': 'application/json'},
     success: function (res) {
-      leadScoreReponse = res;
-      console.log("RES", res)
+      if(callback) {
+        callback(res);
+      }
     },
     error: function (e) {
-      console.log("GOT ERROR");
       console.error(e)
     }
   });
-}
-
+}  
