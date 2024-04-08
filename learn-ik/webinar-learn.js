070cdb956e2e19392e6935b061d71b1db21072a8
Ankur <ankur@interviewkickstart.com>
2024-04-08 15:25:40 +0530

    Update webinar-learn.js

--- learn-ik/webinar-learn.js
+++ learn-ik/webinar-learn.js
@@ -11,7 +11,7 @@ function getDeviceType() {
 }
 
 function formattedWebinarDate(webinarData, webinarFormat) {
-  return `<div class="webinar-event-date" bis_skin_checked="1"  data-starttime="${webinarData.start_time}" data-endtime="${webinarData.end_time}" data-invitee_starttime="${webinarData.invitee_start_time}"  data-invitee_endtime="${webinarData.invitee_end_time}" data-name="${webinarData.startDate}" data-webinar_lead_type="${webinarData.webinar_lead_type}">  ${webinarFormat} </div>`
+  return `<div class="webinar-event-date" bis_skin_checked="1"  data-starttime="${webinarData.start_time}" data-endtime="${webinarData.end_time}" data-invitee_starttime="${webinarData.invitee_start_time}"  data-invitee_endtime="${webinarData.invitee_end_time}" data-name="${webinarData.start_time}" data-webinar_lead_type="${webinarData.webinar_lead_type}">  ${webinarFormat} </div>`
 }
 
 $(document).ready((function () {
