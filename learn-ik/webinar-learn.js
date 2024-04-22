309c94f321cf4c65c1155dc9fa433f97b1da8e56
Ankur <ankur@interviewkickstart.com>
2024-04-22 12:52:23 +0530

    Update webinar-learn.js
    remove console eventUpsightDate

--- learn-ik/webinar-learn.js
+++ learn-ik/webinar-learn.js
@@ -37,7 +37,6 @@ $(document).ready((function () {
 
     if (typeof isUpsightReg !== 'undefined') {
       eventUpsightDate = e;
-      console.log("eventUpsightDate", eventUpsightDate);
       function setWebinarFirstTimeSlot() {
         var n = e[0].weekday + ", " + e[0].day + " " + t[parseInt(e[0].month) - 1] + " " + e[0].year + " | " + e[0].hour + ":" + e[0].minute + " " + e[0].am_or_pm;
         webinarSlotDate = formattedWebinarDate(e[0], n);
