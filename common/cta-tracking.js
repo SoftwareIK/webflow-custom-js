12551c4d7b54b97a6823b3495f071e0a7737b501
Ankur <ankur@interviewkickstart.com>
2024-04-24 16:36:18 +0530

    Create cta-tracking.js
    Create a new file for CTA tracking code

---
+++ common/cta-tracking.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+
+  function addIdToElement(selectorPrefix, textPrefix) {
+    $(selectorPrefix).each(function () {
+      let linkText = $(this).text().trim().toLowerCase();
+      let hyphenatedText = linkText.split(' ').join('-');
+      let linkID = textPrefix + hyphenatedText + "-link_redirect-new-page";
+      $(this).attr('id', linkID);
+    });
+  }
+  addIdToElement('.eng-role-link-v2', "course-list_course-");
+  addIdToElement('.indian-footer-links', "footer-");
+  addIdToElement('.new-footer-link', "footer-");
+
+  $("a").on("click", function () {
+    var clickID = $(this).attr('id');
+    var timestamp = new Date().getTime();
+    const trackingObj = {
+      "page_value_url": window.location.href,
+      "student_uuid": vgid,
+      "timestamp": timestamp,
+      "ClickID": clickID,
+    }
+    var trackingData = JSON.stringify(trackingObj);
+    console.log("trackingData:", trackingData);
+  });
+});
