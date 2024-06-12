8f6b9eeccd433f02567cae9f733af6cb67141a93
kdholakiya <kdholakiya>
2024-06-12 10:49:33 +0530

    UG-1982: formatted code.

--- us-ik/webinar-timer-us.js
+++ us-ik/webinar-timer-us.js
@@ -1,148 +1,193 @@
-/**
- * Minified by jsDelivr using Terser v5.19.2.
- *
- * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
- */
-//https://cdn.jsdelivr.net/gh/sohamtest/ik@main/webinar-timer-us.v1.1.js
 const timerState = {
   currentDate: "",
   nextDate: "",
   currentDateSec: "",
   nextDateSec: "",
 };
-function nextWebinar(e, t, r) {
-  let n = "";
-  webinarSchedule = "IST" == r ? webinarScheduleIndia : webinarScheduleUSA;
-  for (let e = 0; e < webinarSchedule.length; e++)
-    if (webinarSchedule[e]) {
-      if (
-        Date.parse(
-          new Date(`${webinarSchedule[e].date}, ${webinarSchedule[e].time}`)
-        ) > t
-      ) {
-        n = `${webinarSchedule[e].date}, ${webinarSchedule[e].time}`;
+
+function nextWebinar(currentDate, currentWebTime, tz) {
+  let nextWebinarDate = "";
+
+  webinarSchedule = tz == "IST" ? webinarScheduleIndia : webinarScheduleUSA;
+
+  for (let idx = 0; idx < webinarSchedule.length; idx++) {
+    if (webinarSchedule[idx]) {
+      const currentDateWeb = Date.parse(
+        new Date(`${webinarSchedule[idx].date}, ${webinarSchedule[idx].time}`)
+      );
+      if (currentDateWeb > currentWebTime) {
+        nextWebinarDate = `${webinarSchedule[idx].date}, ${webinarSchedule[idx].time}`;
         break;
       }
     }
-  return n;
+  }
+  return nextWebinarDate;
 }
-function initStates(e) {
-  (timerState.currentDate = new Date().toLocaleString("en-US", {
-    timeZone: e,
-  })),
-    (timerState.currentDateSec = Date.parse(timerState.currentDate)),
-    (timerState.nextDate = nextWebinar(
-      timerState.currentDate.split(",")[0],
-      timerState.currentDateSec,
-      e
-    )),
-    "" !== timerState.nextDate &&
-      (timerState.nextDateSec = Date.parse(timerState.nextDate));
+
+function initStates(tz) {
+  timerState.currentDate = new Date().toLocaleString("en-US", { timeZone: tz });
+  timerState.currentDateSec = Date.parse(timerState.currentDate);
+  timerState.nextDate = nextWebinar(
+    timerState.currentDate.split(",")[0],
+    timerState.currentDateSec,
+    tz
+  );
+  if (timerState.nextDate !== "") {
+    timerState.nextDateSec = Date.parse(timerState.nextDate);
+  }
 }
-function unitCount(e) {
-  return String(Number.parseInt(e / 10)) + String(e % 10);
+
+function unitCount(unit) {
+  const toTens = () => String(Number.parseInt(unit / 10));
+  const toOnes = () => String(unit % 10);
+  return toTens() + toOnes();
 }
+
 function unitaryCountHandler() {
-  parseInt(
-    document.querySelector(".webinar__timer--days > .webinar__timer--count")
-      .textContent
-  ) <= 1 &&
+  if (
+    parseInt(
+      document.querySelector(".webinar__timer--days > .webinar__timer--count")
+        .textContent
+    ) <= 1
+  ) {
     document
       .querySelectorAll(".webinar__timer--days > .webinar__timer--label")
-      .forEach((e) => {
-        e.textContent = "Day";
-      }),
+      .forEach((timer) => {
+        timer.textContent = "Day";
+      });
+  }
+  if (
     parseInt(
       document.querySelector(".webinar__timer--hours > .webinar__timer--count")
         .textContent
-    ) <= 1 &&
-      document
-        .querySelectorAll(".webinar__timer--hours > .webinar__timer--label")
-        .forEach((e) => {
-          e.textContent = "Hr";
-        }),
+    ) <= 1
+  ) {
+    document
+      .querySelectorAll(".webinar__timer--hours > .webinar__timer--label")
+      .forEach((timer) => {
+        timer.textContent = "Hr";
+      });
+  }
+  if (
     parseInt(
       document.querySelector(".webinar__timer--mins > .webinar__timer--count")
         .textContent
-    ) <= 1 &&
-      document
-        .querySelectorAll(".webinar__timer--mins > .webinar__timer--label")
-        .forEach((e) => {
-          e.textContent = "Min";
-        }),
+    ) <= 1
+  ) {
+    document
+      .querySelectorAll(".webinar__timer--mins > .webinar__timer--label")
+      .forEach((timer) => {
+        timer.textContent = "Min";
+      });
+  }
+  if (
     parseInt(
       document.querySelector(".webinar__timer--secs > .webinar__timer--count")
         .textContent
-    ) <= 1 &&
-      document
-        .querySelectorAll(".webinar__timer--secs > .webinar__timer--label")
-        .forEach((e) => {
-          e.textContent = "Sec";
-        });
+    ) <= 1
+  ) {
+    document
+      .querySelectorAll(".webinar__timer--secs > .webinar__timer--label")
+      .forEach((timer) => {
+        timer.textContent = "Sec";
+      });
+  }
 }
-function updateTimerUI(e, t, r, n) {
+
+function updateTimerUI(day, hrs, min, sec) {
   document
     .querySelectorAll(".webinar__timer--days > .webinar__timer--count")
-    .forEach((t) => {
-      t.textContent = e;
-    }),
-    document
-      .querySelectorAll(".webinar__timer--hours > .webinar__timer--count")
-      .forEach((e) => {
-        e.textContent = t;
-      }),
-    document
-      .querySelectorAll(".webinar__timer--mins > .webinar__timer--count")
-      .forEach((e) => {
-        e.textContent = r;
-      }),
-    document
-      .querySelectorAll(".webinar__timer--secs > .webinar__timer--count")
-      .forEach((e) => {
-        e.textContent = n;
-      }),
-    unitaryCountHandler();
+    .forEach((timer) => {
+      timer.textContent = day;
+    });
+  document
+    .querySelectorAll(".webinar__timer--hours > .webinar__timer--count")
+    .forEach((timer) => {
+      timer.textContent = hrs;
+    });
+  document
+    .querySelectorAll(".webinar__timer--mins > .webinar__timer--count")
+    .forEach((timer) => {
+      timer.textContent = min;
+    });
+  document
+    .querySelectorAll(".webinar__timer--secs > .webinar__timer--count")
+    .forEach((timer) => {
+      timer.textContent = sec;
+    });
+
+  unitaryCountHandler();
 }
-function TimerHandler(e) {
-  initStates(e);
-  const t = setInterval(() => {
-    "" === timerState.nextDate && clearInterval(t),
-      (timerState.currentDate = new Date().toLocaleString("en-US", {
-        timeZone: e,
-      })),
-      (timerState.currentDateSec = Date.parse(timerState.currentDate));
-    const r = timerState.nextDateSec - timerState.currentDateSec;
-    if ("" == timerState.nextDateSec || null == timerState.nextDateSec)
-      return (
-        null != document.querySelector(".webinar__nav-timer") &&
-          (document.querySelector(".webinar__nav-timer").style.display =
-            "none"),
-        null != document.querySelector(".webinar__timer") &&
-          (document.querySelector(".webinar__timer").style.display = "none"),
-        $(".webinar__timer-nav").css("display", "none"),
-        void $(".webinar__timer").css("display", "none")
-      );
-    const n = Math.floor(r / 864e5),
-      a = Math.floor((r % 864e5) / 36e5),
-      i = Math.floor((r % 36e5) / 6e4),
-      o = Math.floor((r % 6e4) / 1e3);
-    updateTimerUI(unitCount(n), unitCount(a), unitCount(i), unitCount(o)),
-      r <= 0 &&
-        (initStates(e),
-        "" === timerState.nextDate &&
-          (clearInterval(t),
-          document.querySelectorAll(".webinar__timer").forEach((e) => {
-            e.classList.add("is-hidden");
-          })));
-  }, 1e3);
+
+function TimerHandler(tz) {
+  // initialize states
+  initStates(tz);
+
+  // start timer
+  const webinarTimer = setInterval(() => {
+    if (timerState.nextDate === "") {
+      clearInterval(webinarTimer);
+    }
+    timerState.currentDate = new Date().toLocaleString("en-US", {
+      timeZone: tz,
+    });
+    timerState.currentDateSec = Date.parse(timerState.currentDate);
+
+    const distanceCount = timerState.nextDateSec - timerState.currentDateSec;
+
+    if (timerState.nextDateSec == "" || timerState.nextDateSec == null) {
+      if (document.querySelector(".webinar__nav-timer") != null) {
+        document.querySelector(".webinar__nav-timer").style.display = "none";
+      }
+
+      if (document.querySelector(".webinar__timer") != null) {
+        document.querySelector(".webinar__timer").style.display = "none";
+      }
+
+      $(".webinar__timer-nav").css("display", "none");
+      $(".webinar__timer").css("display", "none");
+
+      return;
+    }
+
+    const day = Math.floor(distanceCount / (1000 * 60 * 60 * 24));
+    const hrs = Math.floor(
+      (distanceCount % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const min = Math.floor((distanceCount % (1000 * 60 * 60)) / (1000 * 60));
+    const sec = Math.floor((distanceCount % (1000 * 60)) / 1000);
+
+    // separating into tens and ones
+
+    const dayCount = unitCount(day);
+    const hrCount = unitCount(hrs);
+    const minCount = unitCount(min);
+    const secCount = unitCount(sec);
+
+    // update UI
+    updateTimerUI(dayCount, hrCount, minCount, secCount);
+
+    if (distanceCount <= 0) {
+      // move timer to next date if reached 0
+      initStates(tz);
+
+      if (timerState.nextDate === "") {
+        clearInterval(webinarTimer);
+        document.querySelectorAll(".webinar__timer").forEach((timer) => {
+          timer.classList.add("is-hidden");
+        });
+      }
+    }
+  }, 1000);
 }
+
 const stickyTimerHandler = () => {
   window.onscroll = () => {
-    null != document.querySelector(".webinar__nav-timer") &&
-      (scrollY > document.querySelector("#numberRoller").offsetTop - 140
-        ? (document.querySelector(".webinar__nav-timer").style.display = "flex")
-        : (document.querySelector(".webinar__nav-timer").style.display =
-            "none"));
+    if (document.querySelector(".webinar__nav-timer") == null) return;
+    if (scrollY > document.querySelector("#numberRoller").offsetTop - 140) {
+      document.querySelector(".webinar__nav-timer").style.display = "flex";
+    } else {
+      document.querySelector(".webinar__nav-timer").style.display = "none";
+    }
   };
 };
-//# sourceMappingURL=/sm/5452c55baa02e545d082679c8b063986300bd578778601169e9a4c8efa3c457f.map
