ebc92e2aad39f7eec9e6a125af59c4cfa00f232d
kdholakiya <kdholakiya>
2024-07-05 18:04:35 +0530

    Aded js file for article specific

--- us-ik/article-specific.js
+++ us-ik/article-specific.js
@@ -1,3 +1,20 @@
 $(document).ready(function () {
- 
+
+  function urlContains(paths) {
+    const lastPathSegment = window.location.pathname.split('/').filter(segment => segment).pop();
+    return paths.some(path => lastPathSegment === path);
+  }
+
+  const listOfSwitchUpArticles = [
+    'preparing-machine-learning-course-interview-kickstart'
+  ];
+
+  if (urlContains(listOfSwitchUpArticles)) {
+    $.getJSON("https://get.geojs.io/v1/ip/geo.json", function (t) { }).done(function (t) {
+      if (t.country != "India" && t.timezone != 'Asia/Kolkata') {
+        webinarType = "SWITCH_UP";
+      }
+    }).fail(function (t) { });
+  }
+
 });
