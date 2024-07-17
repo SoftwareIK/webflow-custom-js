63f323a6950f0d5ef1d12614aa9ca3302c8f86d9
kdholakiya <kdholakiya>
2024-07-17 18:01:47 +0530

    UG-2207: Added article edit flag.

--- us-ik/article-specific.js
+++ us-ik/article-specific.js
@@ -1,15 +1,24 @@
 $(document).ready(function () {
+  var collectionId = $('html').data('wf-collection');
+  var itemSlug = $('html').data('wf-item-slug');
 
-  function urlContains(paths) {
-    const lastPathSegment = window.location.pathname.split('/').filter(segment => segment).pop();
-    return paths.some(path => lastPathSegment === path);
+  var urlParams = new URLSearchParams(window.location.search);
+  if (urlParams.has('update') && urlParams.get('update') === 'true') {
+    if (collectionId && itemSlug) {
+      var newUrl = `https://uplevel.interviewkickstart.com/app/cms/articles/items/${collectionId}/${itemSlug}`;
+      window.location.href = newUrl;
+    }
+  }
+
+  function slugContains(paths) {
+    return paths.some(path => itemSlug === path);
   }
 
   const listOfSwitchUpArticles = [
     'preparing-machine-learning-course-interview-kickstart'
   ];
 
-  if (urlContains(listOfSwitchUpArticles)) {
+  if (slugContains(listOfSwitchUpArticles)) {
     $.getJSON("https://get.geojs.io/v1/ip/geo.json", function (t) { }).done(function (t) {
       if (t.country != "India" && t.timezone != 'Asia/Kolkata') {
         webinarType = "SWITCH_UP";
@@ -18,5 +27,4 @@ $(document).ready(function () {
       }
     }).fail(function (t) { });
   }
-
 });
