28015a1d5052b15585b94450b3d7f974dab7e648
Admin <admin@192.168.1.11>
2024-09-05 18:38:37 +0530

    added pagination logic

---
+++ pagination-logic/pagination.js
@@ -0,0 +1,78 @@
+function createPagination(paginationContainerClass, defaultParam) {
+	const maxVisiblePages = 3;
+	const prevButton = document.querySelector('#previous-page-button-articles');
+	const nextButton = document.querySelector('#next-page-button-articles');
+
+	// Use nextButton as the default control button
+	const controlButton = nextButton || prevButton;
+
+	if (!controlButton) return; // Early exit if no control button is found
+
+	controlButton.classList.add('pagination-button');
+
+	const allQuery = controlButton.href.split('?')[1];
+	const hrefUrlParams = new URLSearchParams(allQuery);
+	const pageKey =
+		Array.from(hrefUrlParams.entries()).find(([key]) =>
+			key.includes('page'),
+		)?.[0] || defaultParam;
+
+	const totalPageCount =
+		parseInt(
+			document
+				.querySelector('.w-page-count')
+				.textContent.split('/')[1]
+				.trim(),
+		) || 0;
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+	const currentPage = parseInt(urlParams.get(pageKey)) || 1;
+
+	const paginationContainer = document.querySelector(
+		`.${paginationContainerClass}`,
+	);
+	paginationContainer.innerHTML = '';
+
+	const emptyElips = document.createElement('div');
+	emptyElips.textContent = '...';
+	emptyElips.className = 'empty-elips';
+
+	const addButton = (text, page) => {
+		const button = document.createElement('a');
+		button.href = `?${pageKey}=${page}`;
+		button.textContent = text;
+		button.className = 'pagination-button';
+		if (page === currentPage) button.classList.add('active');
+		paginationContainer.appendChild(button);
+	};
+
+	// Always add the first page button
+	addButton('1', 1);
+
+	// Add ellipsis if currentPage is far enough from the first visible page
+	if (currentPage > maxVisiblePages + 1) {
+		paginationContainer.appendChild(emptyElips.cloneNode(true));
+	}
+
+	// Calculate the range for visible pages between the first and last
+	const startPage = Math.max(2, currentPage - 2);
+	const endPage = Math.min(totalPageCount - 1, currentPage + 2);
+
+	for (let i = startPage; i <= endPage; i++) {
+		addButton(i, i);
+	}
+
+	// Add ellipsis if there are hidden pages between the last visible and the last page
+	if (endPage < totalPageCount - 1) {
+		paginationContainer.appendChild(emptyElips.cloneNode(true));
+	}
+
+	// Always add the last page button
+	if (totalPageCount > 1) {
+		addButton(totalPageCount.toString(), totalPageCount);
+	}
+}
+// example use
+// createPagination() function take two arg parent class and fallback default param
+// to work this we have to enable pagination from webflow collection
+// document.addEventListener('DOMContentLoaded', createPagination('parent-class', urlDefaultParam));
