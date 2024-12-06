8b43da80dfcf0ba8c9bd4d6b2a480b92ea7a780e
Admin <admin@192.168.1.3>
2024-12-06 17:42:31 +0530

    comented daynamic date logic

--- learn-ik/short-form-page-skip-2-steps.js
+++ learn-ik/short-form-page-skip-2-steps.js
@@ -450,16 +450,16 @@ $(document).ready(function () {
       $("#v2-lname").val(lastName || firstName);
     }
   });
-
-    const radioInput = $('input[name="v2-slots-radio"]:checked')
-    const day = radioInput.data('day');
-    const date = radioInput.data('date');
-    const time = radioInput.data('time');
-    const month = getMonthName(radioInput.data('month'))
-    SELECTED_SLOT['day'] = day;
-    SELECTED_SLOT['date'] = date;
-    SELECTED_SLOT['time'] = time;
-    SELECTED_SLOT['month'] = month;
+    // if you want daynammic date for single slot then uncomment code below
+    // const radioInput = $('input[name="v2-slots-radio"]:checked')
+    // const day = radioInput.data('day');
+    // const date = radioInput.data('date');
+    // const time = radioInput.data('time');
+    // const month = getMonthName(radioInput.data('month'))
+    // SELECTED_SLOT['day'] = day;
+    // SELECTED_SLOT['date'] = date;
+    // SELECTED_SLOT['time'] = time;
+    // SELECTED_SLOT['month'] = month;
     $(document).on('click', '.slot-radiobutton', function () {
       $('.time-slot-wrapper').removeClass('selected-slot');
       $(this).siblings('.time-slot-wrapper').addClass('selected-slot');
